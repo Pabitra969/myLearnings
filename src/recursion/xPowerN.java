b302d6c68e4aca3657cccebd7b7a9cb96c83102b
Pabitra969 <pkg2000969@gmail.com>
2022-09-17 15:06:10 +0530

    my code

--- src/recursion/xPowerN.java
+++ src/recursion/xPowerN.java
@@ -2,6 +2,16 @@ package recursion;
 
 public class xPowerN {
     public static void main(String[] args) {
+        int x = 2;
+        int n = 4;
+        System.out.println(power(x,n));
+    }
 
+    public static int power (int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int xnm1 = power(x, n-1);
+        return x * xnm1;
     }
 }
