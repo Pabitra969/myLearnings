2fdc822ef3d242341a4daefbd36fc762eb28e4e8
Pabitra969 <pkg2000969@gmail.com>
2022-10-24 02:46:24 +0530

    my code

---
+++ src/backtracking/find_permutation.java
@@ -0,0 +1,25 @@
+package backtracking;
+
+import java.sql.SQLOutput;
+
+public class find_permutation {
+    public static void main(String[] args) {
+        String str = "abc";
+        findPermutation(str, "");
+    }
+    public static void findPermutation (String str, String ans) {
+        // base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        // recursion
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            // "abcde" = "ab" + "de"
+            String NewStr = str.substring(0, i) + str.substring(i+1);
+            findPermutation(NewStr, ans+curr);
+        }
+    }
+}
+
