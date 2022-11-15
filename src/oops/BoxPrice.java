9cf72490fc7f44ffb2fedc6a9dd399c04dcbf54b
Pabitra969 <pkg2000969@gmail.com>
2022-11-15 18:28:06 +0530

    my code

--- src/oops/BoxPrice.java
+++ src/oops/BoxPrice.java
@@ -10,6 +10,11 @@ public class BoxPrice extends BoxWeight {
 
     BoxPrice(BoxPrice other){
         super(other);
+        this.cost = other.cost;
+    }
 
+    public BoxPrice(double l, double h, double w, double weight, double cost) {
+        super(l, h, w, weight);
+        this.cost = cost;
     }
 }
