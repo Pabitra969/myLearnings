64c3c9fc4025a0580e74476a6c381041761d37bb
Pabitra969 <pkg2000969@gmail.com>
2023-01-04 17:43:16 +0530

    my code

--- src/oops/inheritance/Main2.java
+++ src/oops/inheritance/Main2.java
@@ -1,11 +1,12 @@
 package oops.inheritance;
 
-import oops.AccessControl.A;
+//import oops.AccessControl.A;
 
 public class Main2 {
     public static void main(String[] args) {
-        A obj1 = new A(12, "Shyam");
+//        A obj1 = new A(12, "Shyam");
 
 //        obj1.num;
+//        I want to know how the version control works
     }
 }
